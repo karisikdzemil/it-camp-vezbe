e8154e16cf608308b6e1140ae161a80726fdd4e8
karisikdzemil <karisikdzemil@gmail.com>
2022-05-18 20:59:12 +0200

    while petlja

---
+++ cas 20/app.js
@@ -0,0 +1,180 @@
+//Primitive data types:
+
+//string:
+// "a234821390sadjlsakd"
+//number:
+// 25
+//boolean:
+// true , false
+
+// +
+// -
+// *
+// /
+// **
+// %
+// https://www.w3schools.com/js/js_arithmetic.asp
+
+// console.log(true && true)
+// console.log(true && false)
+// console.log(false&&true)
+// console.log(false && false)
+
+// console.log(true || true)
+// console.log(true || false)
+// console.log(false || true)
+// console.log(false || false)
+
+//https://www.w3schools.com/js/js_operators.asp
+
+//https://www.youtube.com/watch?v=FSs_JYwnAdI&list=PL0vfts4VzfNixzfaQWwDUg3W5TRbE7CyI&index=2
+//https://www.youtube.com/watch?v=DHjqpvDnNGE
+
+// unos = Number(prompt("enter your num:"));
+
+// unos_2 = Number(prompt("enter your num:"));
+
+
+ 
+// for ( i = unos; i <= unos_2; i++) {
+//    console.log(i)
+// }
+
+// unos = Number(prompt("enter your num:"));
+
+// unos_2 = Number(prompt("enter your num:"));
+
+
+ 
+// for ( i = unos +1; i <= unos_2; i+=2) {
+//    console.log(i)
+//    if (i % 2 === 0){
+//        console.log(i);
+//    }
+// }
+
+// unos = Number(prompt("enter your num:"));
+
+// unos_2 = Number(prompt("enter your num:"));
+
+// sum = 0;
+
+ 
+// for ( i = unos ; i <= unos_2; i++) {
+//     sum += i;
+// }
+// console.log(sum);
+
+// unos = Number(prompt("enter your num:"));
+
+// unos_2 = Number(prompt("enter your num:"));
+
+// sum = 0;
+
+ 
+// for ( i = unos ; i <= unos_2; i++) {
+
+// }
+// console.log(sum);
+
+// kolicina = Number(prompt("enter"));
+// sum = 0
+// for (i = 0; i < kolicina; i++) {
+//     unos =  Number(prompt("enter"));
+//     sum += unos;
+// }
+// console.log(sum);
+
+// x = Number(prompt("unesite broj redova"));
+// str = "";
+// for (i = 0; i <= x; i++) {
+//   str += "*";
+//   console.log(str);
+// }
+
+// x = Number(prompt("unesite broj redova"));
+// str = 1;
+// for (i = 0; i <= x; i++) {
+//     str++ += "0";
+//     // console.log(str);
+//     console.log(str)
+//   }
+// kolicina = Number(prompt("enter"));
+
+// for (i = 1; i <=kolicina; i++) {
+//   str = "";
+//   for (j = 1; j <= i; j++) {
+
+//   }
+//   console.log(str);
+// }
+// vezba
+// x = Number(prompt("unesite broj redova"));
+// str = "    ";
+// for (i = 0; i <= x; i++) {
+//   str = " *  ";
+//   ste += "  "
+//   console.log(str);
+
+
+  // for (j = i; j <= x; j--) {
+
+
+  // }
+
+// kolicina = Number(prompt("enter"));
+
+// broj = "";
+// sum = 0;
+
+// for (i = 1; i <= kolicina; i++)
+
+// x = Number(prompt("unesite broj redova"));
+// str = "";
+// for (i = 0; i <= x; i++) {
+//   str += "*";
+//   console.log(str);
+// }
+
+// unos = Number(prompt("unesite tacan broj"));
+// d = Math.round(Math.random() * 10);
+// while(unos!==d){
+//   unos = Number(prompt("pogresan broj pokusaj ponovo"));
+// }
+
+// unos = Number(prompt("unesite tacan broj"));
+// d = Math.round(Math.random() * 10);
+// while(unos!==d){
+//   if(unos < d){
+//     unos = Number(prompt("broj je manji"));
+//   }else{unos = Number(prompt("broj je veci"));}
+// }
+
+// unos = parseInt(prompt("unesite pozitivan broj"));
+// sum = 0;
+// while(unos >= 0){
+//   sum += unos;
+//   unos = parseInt(prompt("unesite pozitivan broj"));
+// }
+// console.log(sum)
+
+// unos = parseInt(prompt("unesite vase ime"));
+// sum = "END"
+// unos = (prompt("netacno, probajte opet"));
+// while(sum !== unos){
+//   console.log(unos)
+//   unos = (prompt("netacno, probajte opet"));
+// }
+// console.log("uspeo si")
+
+// kolicina = Number(prompt("unesite vase ime"));
+// pronadjeno = 0;
+// broj = 1;
+// while(pronadjeno !== kolicina){
+//   if(broj % 2 === 1){
+//     pronadjeno++
+//     console.log(broj);
+//   }
+//   broj++
+// }
+
---
+++ domaci 13/app.js
@@ -0,0 +1,15 @@
+unos = prompt ('unesi ime')
+
+if (unos < 21 ) {
+    console.log("dobio si 1"  )
+}
+if (unos > 21 > 41) {
+    console.log("dobio si 2"  )
+}
+// if (unos > 61) {
+//     console.log("dobio si 4"  )
+// }
+// if (unos > 81) {
+//     console.log("dobio si 5"  )
+// }
+
